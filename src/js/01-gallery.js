b71b76d0fc12bea398b29947362f6dd82844e9d8
Vechorkina <artdeco.t@i.ua>
2022-01-26 18:37:04 +0200

    пробл.
    пробл

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox.min.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryCardMarkup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+  )
+  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
